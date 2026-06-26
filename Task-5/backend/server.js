d09437942df1239526c22ec3e4f09bd1124cbdb8
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-06-26 18:20:37 +0530

    Complete task 7

--- Task-5/backend/server.js
+++ Task-5/backend/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const cors = require("cors")
+const path = require("path")
 const app = express()
 const port = 3000
 
 app.use(cors())
 app.use(express.json())
+ 
 
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 let students = []
 
--- Task-6/backend/server.js
+++ Task-6/backend/server.js
@@ -6,38 +6,36 @@ const cors = require("cors")
 
 const authRoutes = require("./routes/authroutes")
 const productRoutes = require("./routes/productRoutes")
+const path = require("path")
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+ 
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("✅ MongoDB Connected");
+    console.log("MongoDB Connected");
   })
   .catch((err) => {
-    console.log("❌ Database Error:", err.message);
+    console.log(" Database Error:", err.message);
   });
 
-
- 
-  //ROutes 
   app.use("/api/auth", authRoutes)
   app.use("/api/products", productRoutes)
-
-//TEst routs 
-
-app.get("/", (req, res) => {
-    res.json({message: "Inventory Management API Running"})
-});
-
 
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
